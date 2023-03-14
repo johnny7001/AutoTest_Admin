118c1533a31c3a6d94b4ef57fff901586bb8fd57
johnny7001 <83850546+johnny7001@users.noreply.github.com>
2023-03-14 17:18:32 +0800

    讀取cookie

--- readCookieImage.py
+++ readCookieImage.py
@@ -59,12 +59,13 @@ def getCaptcha(filepath):
 # 讀cookiess
 with open('./ImageCookies.txt','r',encoding='utf-8') as file:
     content = file.read()
-cookies = content.replace(']','').replace('[','')
-print(cookies, type(cookies))
-# driver = open_driver()
-# driver.add_cookie(cookies)
-# driver.get('https://vendor-stage.ecpay.com.tw/MerchantBasicInfo/MerchantBasicInfo')
-# soup = BeautifulSoup(driver.page_source, 'html.parser')
+cookies = content.replace(']','').replace('[','') #　type=str
+# print(cookies, type(cookies))
+driver = open_driver()
+driver.add_cookie(str(cookies))
+driver.get('https://vendor-stage.ecpay.com.tw/MerchantBasicInfo/MerchantBasicInfo')
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+print(soup)
 
 # # 辨識圖片
 # number = getCaptcha('/captcha.png')
