df07287ff1827964f274ce5e15d252f0bb753f28
曾翊展 <johnny@cengyizhandeMacBook-Air.local>
2023-03-14 23:03:05 +0800

    修改 pkg

--- pkg.py
+++ pkg.py
@@ -1,3 +1,5 @@
+from email.mime.text import MIMEText
+import smtplib
 from datetime import datetime, timezone, timedelta
 import os
 from selenium import webdriver
@@ -25,14 +27,19 @@ display.start()
 # AuthNO = os.getenv("AuthNO")
 
 # from apscheduler.schedulers.blocking import BlockingScheduler
+
+
 def message(msg):
     # - |2017-04-09 02:08:55.764256 | 訊息，顯示在這邊
-    current = '{0:%Y-%m-%d %H:%M:%S.%f}'.format(datetime.now(timezone.utc) + timedelta(hours=8))
+    current = '{0:%Y-%m-%d %H:%M:%S.%f}'.format(
+        datetime.now(timezone.utc) + timedelta(hours=8))
     log = "- |" + current + " | " + ": " + str(msg)
     print(log)
     return log
 
 # 執行chrome_driver
+
+
 def open_driver():
     try:
         # Check if the current version of chromedriver exists
@@ -46,9 +53,11 @@ def open_driver():
         # chrome_options.add_argument('--headless') # 無頭
         # chrome_options.add_experimental_option("detach", True)
         # 隱藏selenium的自動控制功能, 防止被偵測
-        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        chrome_options.add_argument(
+            '--disable-blink-features=AutomationControlled')
         # 設置修改selenium的特徵值, 防止被偵測
-        chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        chrome_options.add_experimental_option(
+            'excludeSwitches', ['enable-automation'])
         # GithubAction用
         chrome_options.add_argument('--window-size=1200,1200')
         chrome_options.add_argument('--ignore-certificate-errors')
@@ -60,39 +69,46 @@ def open_driver():
         logging.info(f'chrome_diver開啟失敗, 錯誤碼: {err}')
 
 # 辨識圖片
+
+
 def getCaptcha(filepath):
     try:
         # now_path = os.getcwd()  # 查看現在在哪一個路徑
         # PATH = now_path + r"\Tesseract-OCR\tesseract.exe"
-        PATH = "/home/runner/work/AutoTest_Admin/Tesseract-OCR/tesseract.exe"
+        PATH = "./Tesseract-OCR/tesseract.exe"
         print('tesseract執行檔路徑: '+PATH)
         # pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
         pytesseract.pytesseract.tesseract_cmd = PATH
-        img = Image.open(f"{filepath}")
+        img = Image.open(f"./{filepath}")
         # img.show()
-        imgResult = pytesseract.image_to_string(img, lang="eng").strip() # type=str
+        imgResult = pytesseract.image_to_string(
+            img, lang="eng").strip()  # type=str
         return imgResult
     except Exception as err:
         return logging.info(f"識別失敗, 錯誤訊息: {err}")
 
 # 下載圖片
+
+
 def downloadImg(captchaBase64, filename):
     try:
         # 先將 data Url 前綴 (data:image/png;base64) 去除，再將 base64 資料轉為 bytes
         i = base64.b64decode(captchaBase64.split(',')[1])
         i = io.BytesIO(i)
-        i = mpimg.imread(i, format='PNG') # 讀檔會失敗
+        i = mpimg.imread(i, format='PNG')  # 讀檔會失敗
         # # 顯示驗證碼
         plt.imshow(i)
         # 不顯示xy軸及邊框
         plt.axis('off')
         # plt.show()
-        plt.savefig(f"/home/runner/work/AutoTest_Admin/{filename}")
+        plt.savefig(f"./{filename}")
         print(f'{filename}圖片下載')
     except Exception as err:
         logging.info(f'圖片讀檔失敗, 錯誤訊息: {err}')
 
 # 用js處理base64
+
+
 def catchBase64(driver):
     try:
         # 用js處理圖片
@@ -112,19 +128,24 @@ def catchBase64(driver):
     except Exception as err:
         logging.info(f'catchBase64失敗, 錯誤訊息: {err}')
 
-Account="Stage2000214"
-Password="test1234"
-AuthNO="53538851"
+
+Account = "Stage2000214"
+Password = "test1234"
+AuthNO = "53538851"
 
 # 後台登入
+
+
 def AdminLogin(driver):
-    driver.get('https://vendor-stage.ecpay.com.tw/MerchantBasicInfo/MerchantBasicInfo')
+    driver.get(
+        'https://vendor-stage.ecpay.com.tw/MerchantBasicInfo/MerchantBasicInfo')
     # 視窗最大化
     # driver.maximize_window()
     # 點我登入
     driver.find_element(By.XPATH, '//*[@id="ecpayLogin"]').click()
     # 輸入帳號
-    driver.find_element(By.XPATH, '//*[@id="Account"]').send_keys(Account)
+    driver.find_element(
+        By.XPATH, '//*[@id="Account"]').send_keys("Stage2000214")
 
     # 判斷是否有彈跳視窗, 若有彈跳視窗表示登入失敗
     while True:
@@ -132,16 +153,18 @@ def AdminLogin(driver):
         driver.find_element(By.XPATH, '//*[@id="LoginAllPay"]').click()
         driver.implicitly_wait(10)
         # 輸入密碼
-        driver.find_element(By.XPATH, '//*[@id="Password"]').send_keys(Password)
+        driver.find_element(
+            By.XPATH, '//*[@id="Password"]').send_keys("test1234")
         # 輸入統一編號
-        driver.find_element(By.XPATH, '//*[@id="AuthNO"]').send_keys(AuthNO)
+        driver.find_element(
+            By.XPATH, '//*[@id="AuthNO"]').send_keys("53538851")
         filename = "captcha.png"
 
         captchaBase64 = catchBase64(driver)
         downloadImg(captchaBase64, filename)
 
         # 處理圖片, 取得驗證碼
-        number = getCaptcha(f'/home/runner/work/AutoTest_Admin/{filename}')
+        number = getCaptcha(filename)
         print(f'取得驗證碼: {number}')
 
         while number.isdigit() != True or len(number) < 4:
@@ -158,7 +181,8 @@ def AdminLogin(driver):
             time.sleep(1)
 
         # 輸入驗證碼
-        driver.find_element(By.XPATH, '//*[@id="allpayCaptchaValue"]').send_keys(number)
+        driver.find_element(
+            By.XPATH, '//*[@id="allpayCaptchaValue"]').send_keys(number)
         # 點擊登入
         driver.find_element(By.XPATH, '//*[@id="LoginAllPay"]').click()
         time.sleep(2)
@@ -172,17 +196,19 @@ def AdminLogin(driver):
     return driver
 
 # 保存cookie且登入
+
+
 def cookieLogin(driver):
     # 保存cookie
-    ListCookies = driver.get_cookies() # type = list
+    ListCookies = driver.get_cookies()  # type = list
     for cookie in ListCookies:
         driver.add_cookie(cookie)
-    driver.get('https://vendor-stage.ecpay.com.tw/MerchantBasicInfo/MerchantBasicInfo')
+    driver.get(
+        'https://vendor-stage.ecpay.com.tw/MerchantBasicInfo/MerchantBasicInfo')
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     return soup
 
-from email.mime.text import MIMEText
-import smtplib
+
 def sendEmail():
     # SMTP
     account = "theforeverwen@gmail.com"
