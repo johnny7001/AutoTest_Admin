d157d8374d7998eead6127259f77c0e916923f15
曾翊展 <johnny@cengyizhandeMacBook-Air.local>
2023-03-15 23:12:11 +0800

    upload shg

---
+++ tesseractUbuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get install g++
+sudo apt-get install autoconf automake libtool
+sudo apt-get install pkg-config
+sudo apt-get install libpng-dev
+sudo apt-get install libjpeg8-dev
+sudo apt-get install libtiff5-dev
+sudo apt-get install zlib1g-dev
+
+sudo apt-get install libleptonica-dev
+
+wget https://github.com/tesseract-ocr/tesseract/archive/3.05.02.tar.gz
+
+### Compile and run
+./autogen.sh
+./configure --prefix=/usr/share/tesseract-ocr/
+make
+make install
+ls -Rl /usr/share/tesseract-ocr/
+### Set PATH (Optional)
+echo "PATH=/usr/share/tesseract-ocr/bin:\$PATH" >> ~/.profile
+
+# $ make training
+# $ sudo make training-install
