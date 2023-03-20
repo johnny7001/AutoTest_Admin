94b67165398e7af7399bb8354979f4b63054c2cc
johnny7001 <83850546+johnny7001@users.noreply.github.com>
2023-03-20 09:46:09 +0800

    merge本地分支

--- autoTest_Semicolon.py
+++ autoTest_Semicolon.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
 import logging
 import time
-from pkg import open_driver,AdminLogin,cookieLogin
+from pkg import open_driver, AdminLogin, cookieLogin, sendEmail
 from selenium.webdriver.common.by import By
 
 FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 logging.basicConfig(level=logging.INFO, filename=f'autoTest_Semicolon.txt', filemode='a+', format=FORMAT,
                     encoding='utf-8')
 
+# 紀錄信件內容
+mailContent = ''
 driver = open_driver()
 # 登入後台
 driver = AdminLogin(driver)
@@ -16,7 +18,7 @@ driver.implicitly_wait(30)
 soup = cookieLogin(driver)
 
 # 顯示修改之前的電子信箱
-while soup.find('input',id='ContactEmail') is None:
+while soup.find('input', id='ContactEmail') is None:
     print('沒抓到聯絡人信箱, 重新登入')
     # 重新登入
     driver = AdminLogin(driver)
@@ -26,6 +28,7 @@ while soup.find('input',id='ContactEmail') is None:
 contactEmail = soup.find('input', id='ContactEmail').get('value')
 print(f'修改之前的電子信箱:{contactEmail}')
 logging.info(f'修改之前的電子信箱:{contactEmail}')
+mailContent += f'修改之前的電子信箱:{contactEmail}\n'
 
 # 產生隨機碼
 mailRandomNum = str(int(time.time()))
@@ -37,6 +40,7 @@ driver.find_element(By.XPATH, '//*[@id="ContactEmail"]').clear()
 driver.find_element(By.XPATH, '//*[@id="ContactEmail"]').send_keys(InputEmail)
 print(f'輸入的電子信箱:{InputEmail}')
 logging.info(f'輸入的電子信箱:{InputEmail}')
+mailContent += f'輸入的電子信箱:{InputEmail}\n'
 # 點擊修改
 driver.find_element(By.XPATH, '//*[@id="btUpdata"]').click()
 time.sleep(2)
@@ -46,18 +50,20 @@ try:
     if alert.text != '更新成功':
         print(f'彈跳視窗回傳訊息: {alert.text},請檢查是否異常-Fail')
         logging.info(f'彈跳視窗回傳訊息: {alert.text},請檢查是否異常-Fail')
+        mailContent += f'彈跳視窗回傳訊息: {alert.text},請檢查是否異常-Fail\n'
     else:
         print(f'彈跳視窗回傳訊息: {alert.text}-Pass')
         logging.info(f'彈跳視窗回傳訊息: {alert.text}-Pass')
+        mailContent += f'彈跳視窗回傳訊息: {alert.text}-Pass\n'
     driver.switch_to.alert.accept()  # 點擊確定按鈕
 except Exception as err:
     logging.info(f'未出現彈跳視窗訊息: {err}')
-
+    mailContent += f'未出現彈跳視窗訊息: {err}\n'
 # 重新整理頁面
 driver.refresh()
 time.sleep(1)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
-while soup.find('input',id='ContactEmail') is None:
+while soup.find('input', id='ContactEmail') is None:
     print('沒抓到聯絡人信箱, 重新整理頁面')
     # 重新登入
     driver = AdminLogin(driver)
@@ -67,11 +73,16 @@ while soup.find('input',id='ContactEmail') is None:
 editContactEmail = soup.find('input', id='ContactEmail').get('value')
 print(f'檢查信箱內容是否有被更修改:{editContactEmail}')
 logging.info(f'檢查信箱內容是否有被更修改:{editContactEmail}')
+mailContent += f'檢查信箱內容是否有被更修改:{editContactEmail}\n'
 if contactEmail == editContactEmail:
     print('信箱內容未被修改,請檢查是否異常-Fail')
     logging.info('信箱內容未被修改,請檢查是否異常-Fail')
+    mailContent += '信箱內容未被修改,請檢查是否異常-Fail\n'
 else:
     print('信箱內容更新成功-Pass')
     logging.info('信箱內容更新成功-Pass')
+    mailContent += '信箱內容更新成功-Pass\n'
 logging.info("="*150)
 driver.close()
+# 寄信通知
+sendEmail(mailTitle='autoTest_Semicolon', mailContent=mailContent)
